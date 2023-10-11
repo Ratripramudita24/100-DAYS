71d6f804e7a4c76fda872a8e56ba794ac8681c11
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-11 18:58:58 +0800

    day 3

---
+++ Days.java
@@ -0,0 +1,30 @@
+package pkg100.days;
+public class Days {
+    public static void main(String[] args) {
+        String nama, gedung, gedung1, gedung2, gedung3;
+        char akreditasi;
+        byte suhu;
+        boolean status;
+    
+        nama = " diza";
+        gedung = "sekolah";
+        gedung2 = "foto copy";
+        gedung3 = "mini market";
+        akreditasi = 'A';
+        suhu = -1;
+        status= true;
+        
+        System.out.println(gedung + " tempat"+ nama + " belajar ");
+        System.out.println("merupakan " + gedung + " dengan akreditasi: " + akreditasi);
+        System.out.println("di sekitar lingkungan " + gedung + nama);
+        System.out.println("terdapat tempat " + gedung2 + " dan " + gedung3);
+        System.out.println("dan saat suhu turun hingga " + suhu + " derajat");
+        System.out.println(gedung + " akan di liburkan");
+        System.out.println("- - - - - - - - - - - - - - - - -");
+        System.out.println("status informasi: " + status);
+        
+        
+        // TODO code application logic here
+    }
+    
+}
