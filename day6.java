05d03bca2b0180c4b1fbd837b4c0244cfe50ef0a
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-14 10:54:53 +0800

    day 6

---
+++ day6.java
@@ -0,0 +1,39 @@
+package pkg100.days;
+public class day6 {
+    public static void main(String[] args) {
+    int nilai, nilai2, hasil;
+    nilai = 2;
+    nilai2 = 4;
+    nilai2*=2/1;
+        System.out.println("hasil: " + nilai2);
+        
+    int nilaiX, nilaiY, nilaiZ, nilaiI;
+    boolean jawaban;
+    nilaiX = 12;
+    nilaiY = 10;
+    nilaiZ = 9;
+    nilaiI = 10;
+    
+    jawaban = nilaiX < nilaiY;
+        System.out.println(jawaban);
+        
+        jawaban = nilaiX > nilaiZ;
+        System.out.println(jawaban);
+        
+        jawaban = nilaiY <= nilaiX;
+        System.out.println(jawaban);
+        
+        jawaban = nilaiZ>= nilaiX;
+        System.out.println(jawaban);
+        
+        jawaban = nilaiI == nilaiY;
+        System.out.println(jawaban);
+        
+        jawaban = nilaiY != nilaiI;
+        System.out.println(jawaban);
+            
+            
+    
+    }
+
+}
