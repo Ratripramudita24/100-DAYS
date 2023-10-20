42dd0bf37e5870bc97ae7a7f656cfbcb2a5e289e
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-20 23:04:16 +0800

    day 12

--- day12_percabangan.java
+++ day12_percabangan.java
@@ -1,14 +1,15 @@
 package pkg100.days;
 import java.util.Scanner;
 public class day12_percabangan {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
         
         int gaji, PTKP, PKP;
-        PTKP = 54000000;    //pendapatan tidak kena pajak
+        PTKP = 54000000;   //pendapatan tidak kena pajak(PTKP)
         System.out.println("gaji: ");
-        gaji = input.nextInt();
-        gaji *= 12;
+        gaji = input.nextInt();  //gaji perbulan
+        gaji *= 12;   //gaji per-bulan dikali 12 bulan
+        
         if(gaji <= 50000000){
             System.out.println("kena pajak 5%");
         }else if (gaji > 250000000) {
@@ -16,7 +17,7 @@ public class day12_percabangan {
         } else {
             System.out.println("kena pajak 15%");
         }
-        PKP = gaji - PTKP;  //pkp(penghasilan kena pajak)
+        PKP = gaji - PTKP;  //penghasilan kena pajak (PKP)
         System.out.println("PKP: Rp." + PKP);
     }
     
