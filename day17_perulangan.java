81d3c424ec0569fca841649115234bc5bb9b9b2a
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-25 18:36:08 +0800

    day 17

---
+++ day17_perulangan.java
@@ -0,0 +1,10 @@
+package pkg100.days;
+
+public class day17_perulangan {
+    public static void main(String[] args) {
+       
+        for (int i = 3; i <= 15; i+= 4) {
+            System.out.println(i);
+        }
+    }
+}    
