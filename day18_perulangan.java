031aa9bf0fb091584d2f870e12da225e717b0318
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-26 19:28:48 +0800

    day 18

---
+++ day18_perulangan.java
@@ -0,0 +1,11 @@
+package pkg100.days;
+public class day18_perulangan {
+    public static void main(String[] args) {
+        
+        
+        for (int x = 5; x <= 13; x++) {
+            System.out.println("halo, saya WEi");
+        }    
+    }   
+    
+}
