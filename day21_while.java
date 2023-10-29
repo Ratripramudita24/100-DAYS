09bf82360510b966f916f0e82a8e6a90a124fbe2
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-29 19:10:45 +0800

    day 21

---
+++ day21_while.java
@@ -0,0 +1,11 @@
+package pkg100.days;
+public class day21_while {
+    public static void main(String[] args) {
+        int bilangan = 1;
+        while (bilangan <= 10) {
+            System.out.println("perulangan ke: " + (bilangan + 2));
+            bilangan++;
+        }
+    }
+    
+}
