44551c98112ac30ab54025beb86320c5c8089d4e
Ratripramudita24 <ratripramudita36@gmail.com>
2023-10-30 17:49:41 +0800

    day 22

---
+++ day22_perulangan.java
@@ -0,0 +1,22 @@
+
+package pkg100.days;
+
+public class day22_perulangan {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("menghasilkan angka genap");
+        for (int i = 2; i < 35; i+=2) {
+            System.out.println("i = " + i);
+            
+        }
+        System.out.println("- - - - - - - - - - - - - - ");
+        System.out.println("                            ");
+        System.out.println("menghasilkan angka ganjil");
+        for (int y = 1; y <= 35; y+=2) {
+            System.out.println("Y: "+ y);
+            
+        }
+    }
+    
+}
