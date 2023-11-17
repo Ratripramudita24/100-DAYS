1ad914f80b1286f2d74f8d6765ed84bf0458236b
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-17 18:43:30 +0800

    day 40

---
+++ day40_for.java
@@ -0,0 +1,14 @@
+package pkg100.days;
+public class day40_for {
+    public static void main(String[] args) {
+        
+        int angka = 5;
+        for (int i = 1; i <= angka ; i++) {
+            for (int j = 1; j <= angka; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
