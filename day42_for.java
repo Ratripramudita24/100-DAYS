0e913d89105eb8de5f05f8a89ed991bec504b8e4
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-19 20:25:32 +0800

    day 42

---
+++ day42_for.java
@@ -0,0 +1,11 @@
+package pkg100.days;
+public class day42_for {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print(j);
+            }System.out.println();
+        }
+    }
+    
+}
