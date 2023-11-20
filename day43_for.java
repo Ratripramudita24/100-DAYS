ad003279a4c87c425b8186ce30005ea4e8bf3aa8
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-20 18:35:38 +0800

    day 43

---
+++ day43_for.java
@@ -0,0 +1,9 @@
+package pkg100.days;
+public class day43_for {
+    public static void main(String[] args) {
+        for (int i = 3; i < 50; i+=3) {
+            System.out.println("Bilangan kelipatan 3 adalah "+i);
+        }
+    }
+    
+}
