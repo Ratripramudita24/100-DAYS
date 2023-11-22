f77f1221cfa3fbcd91e36ef22ab74516fc3a78be
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-22 20:18:03 +0800

    day 45

---
+++ day45_for.java
@@ -0,0 +1,14 @@
+package pkg100.days;
+
+public class day45_for {
+    public static void main(String[] args) {
+        
+        System.out.println("Perkalian 7");
+        for (int i = 1; i <= 10; i++) {
+            int jawaban = i*7;
+            System.out.println(i+" x 7 = "+jawaban);
+        }
+        
+    }
+    
+}
