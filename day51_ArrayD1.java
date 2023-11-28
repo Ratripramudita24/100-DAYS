00cbd8c893fdde39f9da619d184c5e86780b585d
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-28 18:47:07 +0800

    day 51

---
+++ day51_ArrayD1.java
@@ -0,0 +1,13 @@
+package pkg100.days;
+
+public class day51_ArrayD1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Mencetak array dengan for");
+        int[] nilai = {12,2,13,5,4,6};
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println(nilai[i]);
+        }
+    }
+    
+}
