d13bec8f01b7e6dfe214d0611a9e77d527dba5a5
Ratripramudita24 <ratripramudita36@gmail.com>
2023-11-29 18:45:35 +0800

    day 52

---
+++ day52_ArrayD1.java
@@ -0,0 +1,14 @@
+package pkg100.days;
+
+import java.util.Arrays;
+
+public class day52_ArrayD1 {
+    public static void main(String[] args) {
+        
+        String[] nama = {"Ratri","Ditha","Dikai","Jah"};
+        nama[0] = "Xia";
+        System.out.println(Arrays.toString(nama));
+        
+    }
+    
+}
