8e6332b66c2b310f8f1ce3311e307c996df21a26
Ratripramudita24 <ratripramudita36@gmail.com>
2023-12-01 18:23:39 +0800

    day 54

---
+++ day54_ArrayD1.java
@@ -0,0 +1,17 @@
+package pkg100.days;
+
+import java.util.Arrays;
+
+public class day54_ArrayD1 {
+    public static void main(String[] args) {
+        
+        int[] angka = {23,67,89,20,29,54,26};
+        int i = 0;
+        do{
+            System.out.println(angka[i]);
+            i++;
+        }while(i<angka.length);
+        
+    }
+    
+}
