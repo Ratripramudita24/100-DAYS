2f7f8815571f2ad981e6c81d8bf4e17509e12997
Ratripramudita24 <ratripramudita36@gmail.com>
2023-12-03 20:36:48 +0800

    day 56

---
+++ day56_ArrayD1.java
@@ -0,0 +1,15 @@
+package pkg100.days;
+
+import java.util.Arrays;
+
+public class day56_ArrayD1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Mencari Panjang Array");
+        char[] kelas = {'A','B','C','D','E','F','G','H'};
+        System.out.println(Arrays.toString(kelas));
+        System.out.println("Panjang Array adalah "+kelas.length);
+        
+    }
+    
+}
