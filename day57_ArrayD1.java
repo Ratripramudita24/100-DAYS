ed9c6b6beb716441e9a7721c21b48616900cbae5
Ratripramudita24 <ratripramudita36@gmail.com>
2023-12-04 21:48:19 +0800

    day57

---
+++ day57_ArrayD1.java
@@ -0,0 +1,19 @@
+package pkg100.days;
+
+import java.util.Arrays;
+
+public class day57_ArrayD1 {
+    public static void main(String[] args) {
+        
+        String[] nama = new String[5];
+        nama[0] = "diput";
+        nama[1] = "dita";
+        nama[2] = "ija";
+        nama[3] = "dikai";
+        nama[4] = "Diput";
+        
+        System.out.println(Arrays.toString(nama));
+        
+    }
+    
+}
