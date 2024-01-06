6b9bc3a044a87aba0482cfe35e771ff7471c087e
Ratripramudita24 <ratripramudita36@gmail.com>
2024-01-06 19:57:48 +0800

    day 88

---
+++ day88_ArrayList.java
@@ -0,0 +1,22 @@
+package pkg100.days;
+
+import java.util.ArrayList;
+
+public class day88_ArrayList {
+    public static void main(String[] args) {
+        
+        ArrayList informasi = new ArrayList();
+        informasi.add("Prodi Informatika");
+        informasi.add("Kelas " + 'B');
+        informasi.add("Angkatan " + 23);
+        informasi.add("Semester 2");
+        informasi.add(06.01);
+        informasi.add(24);
+        informasi.add(19.45 + " PM");
+        
+        System.out.println("Isi Array yaitu : ");
+        System.out.println(informasi);
+        
+    }
+    
+}
